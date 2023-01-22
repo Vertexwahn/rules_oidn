66840e27d4b76dbb1b587b4da5213cc559b7e557
Vertexwahn <julian.amann@tum.de>
2023-01-22 21:03:00 +0100

    Enhance example
    GitOrigin-RevId: 61acc8023110a51ce16f66278c6efc56555a5fb6

--- tests/example.cpp
+++ tests/example.cpp
@@ -5,22 +5,29 @@
 
 #include "OpenImageDenoise/oidn.hpp"
 
-#include <iostream>
+#include "imaging/io/io_openexr.h"
+using namespace de_vertexwahn;
 
+#include <iostream>
 using namespace std;
 
 int main() {
     cout << "Hello World!" << endl;
 
+    Image3f noisy = load_image_openexr("testbed/oidn/data/noisy_10spp.exr");
+    Image3f normal = load_image_openexr("testbed/oidn/data/normal_10spp.exr");
+    Image3f albedo = load_image_openexr("testbed/oidn/data/albedo_10spp.exr");
+    Image3f out{noisy.size()};
+
+    float* colorPtr = noisy.data();
+    float* albedoPtr = albedo.data();
+    float* normalPtr = normal.data();
+    float* outputPtr = out.data();
+    int width = out.width();
+    int height = out.height();
+
     oidn::DeviceRef device = oidn::newDevice();
     device.commit();
-
-    float* colorPtr = nullptr;
-    float* albedoPtr = nullptr;
-    float* normalPtr = nullptr;
-    float* outputPtr = nullptr;
-    int width = 0;
-    int height = 0;
 
     // Create a filter for denoising a beauty (color) image using optional auxiliary images too
     oidn::FilterRef filter = device.newFilter("RT"); // generic ray tracing filter
@@ -36,6 +43,9 @@ int main() {
 
     // Check for errors
     const char* errorMessage;
-    if (device.getError(errorMessage) != oidn::Error::None)
+    if (device.getError(errorMessage) != oidn::Error::None) {
         std::cout << "Error: " << errorMessage << std::endl;
+    }
+
+    return 0;
 }
