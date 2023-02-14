f489b9f264d62b64fa47c17bbe32d6f2ee7defec
Vertexwahn <julian.amann@tum.de>
2023-02-14 21:05:46 +0100

    Add denoised images
    GitOrigin-RevId: 8bbf5e5ce077eabbcb13b0e0239c4060057b0a3e

---
+++ tests/data/denoiseIt.bat
@@ -0,0 +1,7 @@
+SET FILE_EXTENSION=exr
+SET PATH_TO_DENOISER=C:\Denoiser_v1.5
+SET OUTPUT_PREFIX=denoised_
+
+for /r %%v in (*.%FILE_EXTENSION%) do %PATH_TO_DENOISER%\Denoiser.exe -i "%%~nv.%FILE_EXTENSION%" -o "%OUTPUT_PREFIX%%%~nv.%FILE_EXTENSION%"
+
+cmd /k
